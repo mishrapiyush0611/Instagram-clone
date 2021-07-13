7b95270f4208f052fff880d8fd7dbfef3c7e6ec7
Piyush Mishra <mishrapiyush0611@gmail.com>
2021-07-13 16:43:47 +0530

    make it better

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app=express()
 const mongoose= require('mongoose')
-const {mongourl}=require('./keys')
+const {mongourl}=require('./config/keys')
+const PORT =process.env.Port ||9000
 mongoose.connect(mongourl,{
     useNewUrlParser: true,
     useUnifiedTopology:true
@@ -16,6 +17,13 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
-app.listen(9000,()=>{
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path=require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+app.listen(PORT,()=>{
     console.log('server is ruuning')
 })
--- Server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    mongourl:"mongodb+srv://piyush:greenwood@2001@cluster0.sjoji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"ffsdbmfdsbdsfbmfdmbsdmb"
-}
